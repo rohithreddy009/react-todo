b92ec7322dc5cfcefea308cfddc0fba0debdb0f3
rohithreddy <yrohithreddy12@gmail.com>
2024-02-15 02:02:15 +0530

    env variables

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-// mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos
-// .env
-mongoose.connect("mongodb+srv://mongodbpractice:123Rohith@cluster0.pvtjavj.mongodb.net/todo-app")
+mongoose.connect(process.env.MONGO_URI)
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
